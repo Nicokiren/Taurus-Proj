e197b9ba84613d37720e0512a6c329d4aecd74f1
victxrreis <victor_rm03@hotmail.com>
2025-05-19 20:15:00 -0300

    Conexão com MySQL

---
+++ backend/Migrations/20250519230350_Migration01.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Taurus.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Telefone = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: true),
+                    Endereco = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Servicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Manutencao = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Limpeza = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    TrocaDeOleo = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Orcamento = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Hora = table.Column<TimeSpan>(type: "time(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servicos", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    PessoaId = table.Column<int>(type: "int", nullable: false),
+                    ServicosId = table.Column<int>(type: "int", nullable: false),
+                    DataHora = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Pessoas_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Servicos_ServicosId",
+                        column: x => x.ServicosId,
+                        principalTable: "Servicos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_PessoaId",
+                table: "Tickets",
+                column: "PessoaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_ServicosId",
+                table: "Tickets",
+                column: "ServicosId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+
+            migrationBuilder.DropTable(
+                name: "Servicos");
+        }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -3,14 +3,16 @@ using TaurusApi_Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Conexão com o MySQL
 builder.Services.AddDbContext<TaurusContext>(options =>
-    options.UseInMemoryDatabase("TaurusInMemoryDb"));
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
